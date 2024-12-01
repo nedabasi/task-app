725f73ff634c9e8d982c268a26fcc01ded82449c
nedabasi <59399428+nedabasi@users.noreply.github.com>
2024-12-01 10:06:28 +0100

    Updated config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans all relevant files in your src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
